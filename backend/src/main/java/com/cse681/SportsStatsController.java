87d0de3ba8c4bdba85a79d2ec195335ac567ad19
Sooraj Akkammadam <sakkammadam@yahoo.com>
2025-03-16 12:17:15 -0700

    Enriched the API response

--- backend/src/main/java/com/cse681/SportsStatsController.java
+++ backend/src/main/java/com/cse681/SportsStatsController.java
@@ -1,19 +1,48 @@
 package com.cse681;
 
+// JSON dependencies
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 // Spring Components
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+// Standard JAVA components
 import java.lang.Exception;
 import java.lang.String;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/api")
 public class SportsStatsController {
+
+    // method to return a JSON Node enriched with necessary information
+    private static JsonNode buildStats(JsonNode teamStats1, JsonNode teamStats2, String matchup){
+        // modify
+        ObjectNode teamStatObj = (ObjectNode) teamStats1;
+        // add match up element -
+        teamStatObj.put("matchup", matchup);
+        // add opponent score
+        teamStatObj.put("opponentScore", teamStats2.get("score").asInt());
+        // add matchResult
+        String matchResult;
+        if (teamStats1.get("score").asInt() > teamStats2.get("score").asInt()){
+            matchResult = "Team Win ";
+        } else {
+            matchResult = "Team Loss";
+        }
+        // add match result
+        teamStatObj.put("matchResult", matchResult);
+        // return
+        return teamStatObj;
+    }
+
     private final TeamDataService teamLookup;
 
     public SportsStatsController(TeamDataService teamLookup){
@@ -36,8 +65,27 @@ public class SportsStatsController {
         // Fetch data
         try{
             String response = InvokeHttpClient.sendRequest(url);
+            // Leverage JSON and parse results
             JsonNode jsonResponse = ParseJson.parse(response);
-            return ResponseEntity.ok(jsonResponse);
+            JsonNode matchUpStats = jsonResponse.get("matchUpStats");
+            List<JsonNode> allTeamStatsArray = new ArrayList<>();
+
+            // Parse response and save it to allTeamStatsArray
+            if(matchUpStats.isArray()){
+                for(JsonNode stat: matchUpStats){
+                    JsonNode homeStats = stat.get("homeStats");
+                    JsonNode visStats = stat.get("visStats");
+                    // add stats to allTeamStatsArray
+                    if (homeStats != null && homeStats.get("teamCode").asInt() == teamId) {
+                        allTeamStatsArray.add(buildStats(homeStats, visStats, "home team"));
+                    }
+                    if (visStats != null && visStats.get("teamCode").asInt() == teamId) {
+                        allTeamStatsArray.add(buildStats(visStats, homeStats, "away team"));
+                    }
+                }
+            }
+            // send parsed response back
+            return ResponseEntity.ok(allTeamStatsArray);
         } catch (Exception e){
             return ResponseEntity.status(500).body("Error fetching stats: "+ e.getMessage());
         }
