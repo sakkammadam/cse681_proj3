d9e2c9efcdcdb710d7472cef572b014e828659f0
Sooraj Akkammadam <sakkammadam@yahoo.com>
2025-03-16 00:13:01 -0700

    Updated back

--- backend/src/main/java/com/cse681/SportsStatsController.java
+++ backend/src/main/java/com/cse681/SportsStatsController.java
@@ -7,14 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import java.lang.Exception;
 import java.lang.String;
 
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000") // Allow CORS request from localhost
 public class SportsStatsController {
     private final TeamDataService teamLookup;
 
