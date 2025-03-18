e1238ce24089dea52f448121f2d7d06ff298854f
Sooraj Akkammadam <sakkammadam@yahoo.com>
2025-03-17 18:09:58 -0700

    npm commands

--- bin/build_frontend.sh
+++ bin/build_frontend.sh
@@ -4,3 +4,8 @@ brew install node
 # create a basic react app
 cd ~/Masters_Syr/2025-winter/cse681_projects/project3/frontend/
 npx create-react-app .
+
+# update package.json
+# npm install
+
+npm start
